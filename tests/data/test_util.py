2d5ca5a3b1b1692463fb92607df3a44614b35440
Shravan <shravanchaudhary07@gmail.com>
2019-08-07 18:55:00 +0530

    test(sdk): added activate and track test

---
+++ tests/data/test_util.py
@@ -0,0 +1,36 @@
+# flake8: noqa
+
+import random
+
+users = [
+    'Ashley',
+    'Bill',
+    'Chris',
+    'Dominic',
+    'Emma',
+    'Faizan',
+    'Gimmy',
+    'Harry',
+    'Ian',
+    'John',
+    'King',
+    'Lisa',
+    'Mona',
+    'Nina',
+    'Olivia',
+    'Pete',
+    'Queen',
+    'Robert',
+    'Sarah',
+    'Tierra',
+    'Una',
+    'Varun',
+    'Will',
+    'Xin',
+    'You',
+    'Zeba',
+    ]
+
+
+def get_random_user():
+    return random.choice(users)
