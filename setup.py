c42f9b5782e3cffe2b074d5a2c49c74447909317
Shravan <shravanchaudhary07@gmail.com>
2019-11-28 17:27:22 +0530

    chore(setup.py): Automatically bumped version from 1.3.0 to 1.3.1

--- setup.py
+++ setup.py
@@ -103,7 +103,7 @@ class PostDevelopCommand(develop):
 
 setup(
     name='vwo-python-sdk',
-    version='1.3.0',
+    version='1.3.1',
     description='Python SDK for VWO server-side A/B Testing',
     long_description='Some issue with twine rendering markdown README.md',
     author='VWO',
