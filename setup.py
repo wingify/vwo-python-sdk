76af4cc1e7667ebccf394a0d7af5a87e1ebbed34
softvar <varun2902@gmail.com>
2023-05-05 03:41:20 +0530

    chore(setup.py): Automatically bumped version from 1.34.0 to 1.34.1

--- setup.py
+++ setup.py
@@ -121,7 +121,7 @@ class PostDevelopCommand(develop):
 
 setup(
     name="vwo-python-sdk",
-    version="1.34.0",
+    version="1.34.1",
     description="Python SDK for VWO server-side A/B Testing",
     long_description=long_description,
     long_description_content_type="text/markdown",
