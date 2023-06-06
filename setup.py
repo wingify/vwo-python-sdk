caba67c9f9299073c94aedb63b5c9c7bed567e35
Rohitesh Dutta <rohitesh.dutta@wingify.com>
2023-06-06 15:03:05 +0530

    chore(setup.py): Automatically bumped version from 1.45.0 to 1.46.0

--- setup.py
+++ setup.py
@@ -121,7 +121,7 @@ class PostDevelopCommand(develop):
 
 setup(
     name="vwo-python-sdk",
-    version="1.45.0",
+    version="1.46.0",
     description="Python SDK for VWO server-side A/B Testing",
     long_description=long_description,
     long_description_content_type="text/markdown",
